ca1da975c21d0b58a5cb8f18ea67a6afd820ae7d
fuyu-sama <yingzhouhong@outlook.com>
2021-06-02 09:34:45 +0800

    Add SCTransform

---
+++ src/R/run-SCTransform.R
@@ -0,0 +1,60 @@
+#! /usr/bin/env Rscript
+
+#
+#                       _oo0oo_
+#                      o8888888o
+#                      88" . "88
+#                      (| -_- |)
+#                      0\  =  /0
+#                    ___/`---'\___
+#                  .' \\|     |// '.
+#                 / \\|||  :  |||// \
+#                / _||||| -:- |||||- \
+#               |   | \\\  -  /// |   |
+#               | \_|  ''\---/''  |_/ |
+#               \  .-\__  '-'  __/-. /
+#             ___'. .'  /--.--\  `. .'___
+#          ."" '<  `.___\_<|>_/___.' >' "".
+#         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+#         \  \ `_.   \_ __\ /__ _/   .-` /  /
+#     =====`-.____`.___ \_____/___.-`___.-'=====
+#                       `=---='
+#
+#
+#     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#               佛祖保佑         永无BUG
+#  Codes are far away from bugs with Buddha's bless
+#
+
+# %% environment config
+HOME <- Sys.getenv("HOME")
+renv::activate(paste0(HOME, "/workspace/mouse-brain-full"))
+library(SC3)
+library(Seurat)
+library(SingleCellExperiment)
+
+args <- commandArgs(trailingOnly = TRUE)
+idx <- args[1]
+set.seed(42)
+sessionInfo()
+
+# %% read data and create Seurat object
+count_path <- paste0(
+    HOME, "/workspace/mouse-brain-full/spaceranger/", idx,
+    "/outs/filtered_feature_bc_matrix/", idx, ".csv"
+)
+save_path <- paste0(
+    HOME, "/workspace/mouse-brain-full/SCT/scale_data/",
+    idx, "_SCTransform.csv"
+)
+
+count_df <- read.csv(count_path, check.names = FALSE, row.names = 1)
+seurat_obj <- CreateSeuratObject(count_df)
+
+# %% SCTransform
+seurat_obj <- SCTransform(
+    seurat_obj,
+    return.only.var.genes = FALSE, verbose = FALSE
+)
+write.csv(seurat_obj[["SCT"]]@scale.data, save_path)
--- src/pbs/run-SCTransform.sh
+++ src/pbs/run-SCTransform.sh
@@ -1,6 +1,6 @@
 PYTHON_PATH=$HOME/workspace/mouse-brain-full/venv/bin/python
 
-cd $HOME/workspace/mouse-brain-full/seurat/log
+cd $HOME/workspace/mouse-brain-full/SCT
 
 for idx in E135A E135B E155A E155B E165A E165B E175A1 E175A2 E175B P0B P0A1 P0A2
 do
@@ -9,9 +9,9 @@ do
         #PBS -N ${idx}-SCTransform
         #PBS -l nodes=comput3:ppn=1
 
-        cd $HOME/workspace/mouse-brain-full/seurat
+        cd $HOME/workspace/mouse-brain-full/SCT
 
         source hdf5-1.12.0.sh
-        Rscript $HOME/workspace/mouse-brain-full/src/R/run-SCTransform.R ${idx}
+        Rscript $HOME/workspace/mouse-brain-full/src/R/run-SCT.R ${idx}
 EOF
 done
