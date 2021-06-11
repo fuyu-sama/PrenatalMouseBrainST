6e2dd8d8faa11d3a98cb73dfe1f263d1212d79af
fuyu-sama <yingzhouhong@outlook.com>
2021-06-11 09:11:59 +0800

    Add de pbs script

---
+++ src/pbs/run-de.sh
@@ -0,0 +1,17 @@
+PYTHON_PATH=$HOME/workspace/mouse-brain-full/venv/bin/python
+
+cd $HOME/workspace/mouse-brain-full/log
+qsub << EOF
+    #PBS -N DE
+    #PBS -l nodes=1:ppn=1
+
+    cd $HOME/workspace/mouse-brain-full
+
+    #Rscript src/R/run-de-region.R
+    #${PYTHON_PATH} src/py/draw-de-region.py
+    for region in cortex hippocampus hypothalamus thalamus
+    do
+        ${PYTHON_PATH} src/py/run-id-transfer.py \
+            results/DE/region-specific/UP-\${region}.csv
+    done
+EOF
