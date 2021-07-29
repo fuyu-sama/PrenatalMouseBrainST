f55203f221ee44dd0d996121eff445c68361fdec
fuyu-sama <yingzhouhong@outlook.com>
2021-07-29 10:51:25 +0800

    Add combat

---
+++ src/py/run-combat.py
@@ -0,0 +1,74 @@
+#! venv/bin/python
+# -*- encoding: utf-8 -*-
+
+#
+#                       _oo0oo_
+#                      o8888888o
+#                      88" . "88
+#                      (| -_- |)
+#                      0\  =  /0
+#                    ___/`---'\___
+#                  .' \\|     |// '.
+#                 / \\|||  :  |||// \
+#                / _||||| -:- |||||- \
+#               |   | \\\  -  /// |   |
+#               | \_|  ''\---/''  |_/ |
+#               \  .-\__  '-'  __/-. /
+#             ___'. .'  /--.--\  `. .'___
+#          ."" '<  `.___\_<|>_/___.' >' "".
+#         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+#         \  \ `_.   \_ __\ /__ _/   .-` /  /
+#     =====`-.____`.___ \_____/___.-`___.-'=====
+#                       `=---='
+#
+#
+#     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#               佛祖保佑         永无BUG
+#  Codes are far away from bugs with Buddha's bless
+#
+
+# %% environment config
+from pathlib import Path
+
+import pandas as pd
+import scanpy as sc
+import session_info
+
+WORKDIR = Path.joinpath(Path.home(), "workspace/mouse-brain-full/")
+session_info.show()
+
+idx_full = {
+    "E135A": "V10M17-100-E135A",
+    "E135B": "V10M17-085-E135B",
+    "E155A": "V10M17-100-E155A",
+    "E155B": "V10M17-085-E155B",
+    "E165A": "V10M17-100-E165A",
+    "E165B": "V10M17-085-E165B",
+    "E175A1": "V10M17-101-E175A1",
+    "E175A2": "V10M17-101-E175A2",
+    "E175B": "V10M17-085-E175B",
+    "P0B": "V10M17-100-P0B",
+    "P0A1": "V10M17-101-P0A1",
+    "P0A2": "V10M17-101-P0A2",
+}
+
+# %% read data
+adata = sc.read_csv(
+    Path.joinpath(WORKDIR, "Data/scale_df/logcpm/full-logcpm-inter.csv")).T
+adata.obs["batch"] = [i.split("_")[0] for i in adata.obs.index]
+
+# %% combat and save
+combat_df = pd.DataFrame(
+    sc.pp.combat(adata, inplace=False),
+    index=adata.obs.index,
+    columns=adata.var.index,
+)
+
+combat_df.T.to_csv(
+    Path.joinpath(WORKDIR, "Data/scale_df/combat/full-combat-inter.csv"))
+for idx in idx_full:
+    write_df = combat_df.T.reindex(
+        columns=[i for i in combat_df.T.columns if idx in i])
+    write_df.to_csv(
+        Path.joinpath(WORKDIR, f"Data/scale_df/combat/{idx}-combat-inter.csv"))
