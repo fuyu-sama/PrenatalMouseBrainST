2c73a8f490f7cdab0a6f09466febf149b7553b75
fuyu-sama <yingzhouhong@outlook.com>
2021-08-28 17:31:25 +0800

    Add dim_redu plot with expression colors

---
+++ src/py/draw-genes-dim_redu.py
@@ -0,0 +1,106 @@
+#! venv/bin/python
+# -*- encoding: utf-8 -*-
+
+#
+#                       _oo0oo_
+#                      o8888888o
+#                      88" . "88
+#                      (| -_- |)
+#                      0\  =  /0
+#                    ___/`---'\___
+#                  .' \\|     |// '.
+#                 / \\|||  :  |||// \
+#                / _||||| -:- |||||- \
+#               |   | \\\  -  /// |   |
+#               | \_|  ''\---/''  |_/ |
+#               \  .-\__  '-'  __/-. /
+#             ___'. .'  /--.--\  `. .'___
+#          ."" '<  `.___\_<|>_/___.' >' "".
+#         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+#         \  \ `_.   \_ __\ /__ _/   .-` /  /
+#     =====`-.____`.___ \_____/___.-`___.-'=====
+#                       `=---='
+#
+#
+#     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#               佛祖保佑         永无BUG
+#  Codes are far away from bugs with Buddha's bless
+#
+
+# %% environment config
+import sys
+from pathlib import Path
+
+import pandas as pd
+import session_info
+from matplotlib import pyplot as plt
+
+import densne
+
+WORKDIR = Path.joinpath(Path.home(), "workspace/mouse-brain-full/")
+session_info.show()
+plt.rcParams.update({"font.size": 24})
+
+idx_full = {
+    "E135A": "V10M17-100-E135A",
+    "E135B": "V10M17-085-E135B",
+    "E155A": "V10M17-100-E155A",
+    "E155B": "V10M17-085-E155B",
+    "E165A": "V10M17-100-E165A",
+    "E165B": "V10M17-085-E165B",
+    "E175A1": "V10M17-101-E175A1",
+    "E175A2": "V10M17-101-E175A2",
+    "E175B": "V10M17-085-E175B",
+    "P0B": "V10M17-100-P0B",
+    "P0A1": "V10M17-101-P0A1",
+    "P0A2": "V10M17-101-P0A2",
+}
+colors = [
+    "#FAEBD7", "#00FFFF", "#FFD700", "#0000FF", "#FF8C00", "#EE82EE",
+    "#9ACD32", "#5F9EA0", "#7FFF00", "#7FFFD4", "#6495ED", "#008B8B",
+    "#B8860B", "#C0C0C0", "#000080", "#D8BFD8", "#00CED1", "#9400D3",
+    "#8E804B", "#0089A7", "#CB1B45", "#FFB6C1", "#00FF00", "#800000",
+    "#376B6D", "#D8BFD8", "#F5F5F5", "#D2691E"
+]
+
+scale_method = sys.argv[1]
+
+# %% read counts
+count_path = Path.joinpath(
+    WORKDIR,
+    f"Data/scale_df/{scale_method}/full-{scale_method}.csv",
+)
+count_full_df = pd.read_csv(
+    count_path,
+    index_col=0,
+    header=0,
+).T
+
+# %% densne
+result_densne = densne.run_densne(count_full_df)[0]
+densne_df = pd.DataFrame(
+    result_densne,
+    index=count_full_df.index,
+    columns=["X", "Y"],
+)
+
+# %% color by region
+for gene in count_full_df.columns:
+    fig, ax = plt.subplots(figsize=(15, 15))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.scatter(
+        densne_df["X"],
+        densne_df["Y"],
+        c=count_full_df[gene],
+        cmap="Reds",
+        s=2,
+    )
+    ax.set_title(f"{gene}")
+    fig.savefig(
+        Path.joinpath(
+            WORKDIR,
+            f"draw_genes/{scale_method}-densne/{gene}.jpg"
+        ))
+    plt.close(fig)
