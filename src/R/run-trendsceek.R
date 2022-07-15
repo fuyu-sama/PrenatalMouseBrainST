e8279d92200374c7ebcb9cfd4a748672232f4212
fuyu-sama <yingzhouhong@outlook.com>
2022-07-15 15:35:46 +0800

    Add trendsceek scripts

---
+++ src/R/run-trendsceek.R
@@ -0,0 +1,47 @@
+#! /usr/bin/env Rscript
+
+#
+#                       _oo0oo_
+#                      o8888888o
+#                      88" . "88
+#                      (| -_- |)
+#                      0\  =  /0
+#                    ___/`---'\___
+#                  .' \\|     |// '.
+#                 / \\|||  :  |||// \
+#                / _||||| -:- |||||- \
+#               |   | \\\  -  /// |   |
+#               | \_|  ''\---/''  |_/ |
+#               \  .-\__  '-'  ___/-. /
+#             ___'. .'  /--.--\  `. .'___
+#          ."" '<  `.___\_<|>_/___.' >' "".
+#         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+#         \  \ `_.   \_ __\ /__ _/   .-` /  /
+#     =====`-.____`.___ \_____/___.-`___.-'=====
+#                       `=---='
+#
+#
+#     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#               佛祖保佑         永无BUG
+#  Codes are far away from bugs with Buddha's bless
+#
+
+# %% environment config
+WORKDIR <- paste0(Sys.getenv("HOME"), "/workspace/mouse-brain-full/")
+renv::activate(WORKDIR)
+library(trendsceek)
+
+args <- commandArgs(trailingOnly = TRUE)
+set.seed(16)
+
+idx <- args[1]
+
+# %% read path
+count_path <- paste0(WORKDIR, "Data/scale_df/raw/", idx, "-raw.csv")
+coor_path <- paste0(WORKDIR, "Data/coor_df/", idx, "-coor.csv")
+
+count_df <- read.csv(count_path, check.names = F, row.names = 1)
+coor_df <- read.csv(coor_path, check.names = F, row.names = 1)
+
+# %%
