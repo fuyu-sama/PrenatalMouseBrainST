81cb8ed00824830387e5b53bfa2f134ac0cadfe2
fuyu-sama <yingzhouhong@outlook.com>
2022-07-16 09:42:41 +0800

    Somde script

---
+++ src/py/run-somde.py
@@ -0,0 +1,84 @@
+#! venv/bin/python
+# -*- encoding: utf-8 -*-
+
+#
+#                       _oo0oo_
+#                      o8888888o
+#                      88" . "88
+#                      (| -_- |)
+#                      0\  =  /0
+#                    ___/`---'\___
+#                  .' \\|     |// '.
+#                 / \\|||  :  |||// \
+#                / _||||| -:- |||||- \
+#               |   | \\\  -  /// |   |
+#               | \_|  ''\---/''  |_/ |
+#               \  .-\__  '-'  __/-. /
+#             ___'. .'  /--.--\  `. .'___
+#          ."" '<  `.___\_<|>_/___.' >' "".
+#         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+#         \  \ `_.   \_ __\ /__ _/   .-` /  /
+#     =====`-.____`.___ \_____/___.-`___.-'=====
+#                       `=---='
+#
+#
+#     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#               佛祖保佑         永无BUG
+#  Codes are far away from bugs with Buddha's bless
+#
+
+# %% environment config
+import sys
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from somde import SomNode
+
+WORKDIR = Path.joinpath(Path.home(), "workspace/mouse-brain-full/")
+plt.rcParams.update({"font.size": 16})
+
+idx_full = {
+    "E135A": "V10M17-100-E135A",
+    "E135B": "V10M17-085-E135B",
+    "E155A": "V10M17-100-E155A",
+    "E155B": "V10M17-085-E155B",
+    "E165A": "V10M17-100-E165A",
+    "E165B": "V10M17-085-E165B",
+    "E175A1": "V10M17-101-E175A1",
+    "E175A2": "V10M17-101-E175A2",
+    # "E175B": "V10M17-085-E175B",
+    # "P0B": "V10M17-100-P0B",
+    "P0A1": "V10M17-101-P0A1",
+    "P0A2": "V10M17-101-P0A2",
+}
+
+try:
+    idx = sys.argv[1]
+except IndexError:
+    idx = "E165A"
+
+# %% read data
+count_path = Path.joinpath(WORKDIR, f"Data/scale_df/raw/{idx}-raw.csv")
+count_df = pd.read_csv(
+    count_path,
+    index_col=0,
+    header=0,
+).T
+count_df = count_df.T[count_df.sum(0) >= 3].T
+
+coor_path = Path.joinpath(WORKDIR, f"Data/coor_df/{idx}-coor.csv")
+coor_df = pd.read_csv(coor_path, index_col=0, header=0)
+
+assert all(count_df.index == coor_df.index)
+
+# %%
+coor_df["total_count"] = count_df.T.sum()
+som = SomNode(coor_df[["X", "Y"]].values.astype(np.float32), 20)
+ndf, ninfo = som.mtx(count_df.T)
+nres = som.norm()
+result, SVnum = som.run()
+result.sort_values(by="qval").to_csv(
+    Path.joinpath(WORKDIR, f"results/somde/{idx}-smode.csv"), )
